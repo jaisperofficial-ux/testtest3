e8437d4babff62aed6356ad91a104f99dac1a43e
jaisperofficial-ux <jaisperofficial@gmail.com>
2025-11-04 11:43:14 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 const repo = process.env.GITHUB_REPOSITORY?.split('/')[1]
 const isGhPages = process.env.GITHUB_PAGES === 'true'
 const base = isGhPages && repo ? `/${repo}/` : '/'
 
-export default defineConfig({ plugins: [react()], base })
+export default defineConfig({
+  plugins: [react()],
+  base,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
